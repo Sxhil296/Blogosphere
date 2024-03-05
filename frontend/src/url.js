426b0dc32c56dcb3e303e97aaaf283b3b4ca57a4
Sahil <leviack629@gmail.com>
2024-03-06 00:34:11 +0530

    url update

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,4 @@
-export const URL = "http://localhost:5000";
-export const IF = "http:///localhost:5000/images/";
+// export const URL = "http://localhost:5000";
+// export const IF = "http:///localhost:5000/images/";
+export const URL = "https://blogosphere-0421.onrender.com";
+export const IF = "https://blogosphere-0421.onrender.com/images/";
