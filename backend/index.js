d3c6373de735353fd3f515b14425a3a426779c22
Sahil <leviack629@gmail.com>
2024-04-02 01:41:57 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "https://sphereofblogs.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
